78c08870efc75794251cf9e3d3033690a2179d06
谦风 <terry.lty@alibaba-inc.com>
2019-03-25 10:52:59 +0800

    disable the last shrink for proguard

---
+++ atlas-gradle-plugin/atlas-plugin/src/main/java/proguard/ProGuard.java
@@ -0,0 +1,612 @@
+/*
+ * ProGuard -- shrinking, optimization, obfuscation, and preverification
+ *             of Java bytecode.
+ *
+ * Copyright (c) 2002-2017 Eric Lafortune @ GuardSquare
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package proguard;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import com.google.common.collect.Lists;
+import com.taobao.android.builder.tools.Profiler;
+import proguard.classfile.ClassConstants;
+import proguard.classfile.ClassPool;
+import proguard.classfile.attribute.visitor.AllAttributeVisitor;
+import proguard.classfile.editor.ClassElementSorter;
+import proguard.classfile.editor.LineNumberTableAttributeTrimmer;
+import proguard.classfile.editor.NamedAttributeDeleter;
+import proguard.classfile.visitor.AllMethodVisitor;
+import proguard.classfile.visitor.ClassPrinter;
+import proguard.classfile.visitor.ClassVersionFilter;
+import proguard.obfuscate.Obfuscator;
+import proguard.optimize.Optimizer;
+import proguard.optimize.peephole.LineNumberLinearizer;
+import proguard.preverify.Preverifier;
+import proguard.preverify.SubroutineInliner;
+import proguard.shrink.Shrinker;
+
+/**
+ * Tool for shrinking, optimizing, obfuscating, and preverifying Java classes.
+ *
+ * @author Eric Lafortune
+ */
+public class ProGuard
+{
+    public static final String VERSION = "ProGuard, version 5.3.3";
+
+    private final Configuration configuration;
+    private       ClassPool     programClassPool = new ClassPool();
+    private final ClassPool     libraryClassPool = new ClassPool();
+
+
+    /**
+     * Creates a new ProGuard object to process jars as specified by the given
+     * configuration.
+     */
+    public ProGuard(Configuration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+
+    /**
+     * Performs all subsequent ProGuard operations.
+     */
+    public void execute() throws IOException
+    {
+        // Disable product dump.
+        configuration.note = Lists.newArrayList("**");
+        configuration.warn = Lists.newArrayList("**");
+        configuration.dump = null;
+
+        Profiler.start("Proguard Profile");
+        Profiler.enter("1. Check");
+        System.out.println(VERSION);
+
+        GPL.check();
+
+        if (configuration.printConfiguration != null)
+        {
+            printConfiguration();
+        }
+
+        new ConfigurationChecker(configuration).check();
+
+        if (configuration.programJars != null     &&
+            configuration.programJars.hasOutput() &&
+            new UpToDateChecker(configuration).check())
+        {
+            return;
+        }
+        Profiler.release();
+
+        Profiler.enter("2. Read input");
+        readInput();
+        Profiler.release();
+
+        Profiler.enter("3. Clear preverification");
+        if (configuration.shrink    ||
+            configuration.optimize  ||
+            configuration.obfuscate ||
+            configuration.preverify)
+        {
+            clearPreverification();
+        }
+        Profiler.release();
+
+        Profiler.enter("3. Initialize");
+        if (configuration.printSeeds != null ||
+            configuration.shrink    ||
+            configuration.optimize  ||
+            configuration.obfuscate ||
+            configuration.preverify)
+        {
+            initialize();
+        }
+        Profiler.release();
+
+        if (configuration.targetClassVersion != 0)
+        {
+            target();
+        }
+
+        if (configuration.printSeeds != null)
+        {
+            printSeeds();
+        }
+
+        Profiler.enter("4. shrink");
+        if (configuration.shrink)
+        {
+            shrink();
+        }
+        Profiler.release();
+
+        if (configuration.preverify)
+        {
+            inlineSubroutines();
+        }
+
+        Profiler.enter("5. optimize");
+        if (configuration.optimize)
+        {
+            for (int optimizationPass = 0;
+                 optimizationPass < configuration.optimizationPasses;
+                 optimizationPass++)
+            {
+                Profiler.enter("optimize-opt " + (optimizationPass + 1));
+                if (!optimize())
+                {
+                    Profiler.release();
+                    // Stop optimizing if the code doesn't improve any further.
+                    break;
+                }
+                Profiler.release();
+
+                Profiler.enter("optimize-shrink " + (optimizationPass + 1));
+                // Shrink again, if we may.
+                if ((optimizationPass < configuration.optimizationPasses - 1) && configuration.shrink)
+                {
+                    // Don't print any usage this time around.
+                    configuration.printUsage       = null;
+                    configuration.whyAreYouKeeping = null;
+
+                    shrink();
+                }
+                Profiler.release();
+            }
+        }
+
+        if (configuration.optimize)
+        {
+            Profiler.enter("optimize-linearizeLineNumbers");
+            linearizeLineNumbers();
+            Profiler.release();
+        }
+        Profiler.release();
+
+        Profiler.enter("6. obfuscate");
+        if (configuration.obfuscate)
+        {
+            obfuscate();
+        }
+        Profiler.release();
+
+        Profiler.enter("7. trim line numbers");
+        if (configuration.optimize)
+        {
+            trimLineNumbers();
+        }
+        Profiler.release();
+
+        Profiler.enter("8. preverify");
+        if (configuration.preverify)
+        {
+            preverify();
+        }
+        Profiler.release();
+
+        Profiler.enter("9. sort class elements");
+        if (configuration.shrink    ||
+            configuration.optimize  ||
+            configuration.obfuscate ||
+            configuration.preverify)
+        {
+            sortClassElements();
+        }
+        Profiler.release();
+
+        Profiler.enter("10. write output");
+        if (configuration.programJars.hasOutput())
+        {
+            writeOutput();
+        }
+        Profiler.release();
+
+        Profiler.enter("11. dump");
+        if (configuration.dump != null)
+        {
+            dump();
+        }
+        Profiler.release();
+
+        Profiler.release();
+        //Profiler.sLogger.warn(Profiler.dump());
+        System.out.println(Profiler.dump());
+    }
+
+
+    /**
+     * Prints out the configuration that ProGuard is using.
+     */
+    private void printConfiguration() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Printing configuration to [" + fileName(configuration.printConfiguration) + "]...");
+        }
+
+        PrintStream ps = createPrintStream(configuration.printConfiguration);
+        try
+        {
+            new ConfigurationWriter(ps).write(configuration);
+        }
+        finally
+        {
+            closePrintStream(ps);
+        }
+    }
+
+
+    /**
+     * Reads the input class files.
+     */
+    private void readInput() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Reading input...");
+        }
+
+        // Fill the program class pool and the library class pool.
+        new InputReader(configuration).execute(programClassPool, libraryClassPool);
+    }
+
+
+    /**
+     * Initializes the cross-references between all classes, performs some
+     * basic checks, and shrinks the library class pool.
+     */
+    private void initialize() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Initializing...");
+        }
+
+        new Initializer(configuration).execute(programClassPool, libraryClassPool);
+    }
+
+
+    /**
+     * Sets that target versions of the program classes.
+     */
+    private void target() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Setting target versions...");
+        }
+
+        new Targeter(configuration).execute(programClassPool);
+    }
+
+
+    /**
+     * Prints out classes and class members that are used as seeds in the
+     * shrinking and obfuscation steps.
+     */
+    private void printSeeds() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Printing kept classes, fields, and methods...");
+        }
+
+        PrintStream ps = createPrintStream(configuration.printSeeds);
+        try
+        {
+            new SeedPrinter(ps).write(configuration, programClassPool, libraryClassPool);
+        }
+        finally
+        {
+            closePrintStream(ps);
+        }
+    }
+
+
+    /**
+     * Performs the shrinking step.
+     */
+    private void shrink() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Shrinking...");
+
+            // We'll print out some explanation, if requested.
+            if (configuration.whyAreYouKeeping != null)
+            {
+                System.out.println("Explaining why classes and class members are being kept...");
+            }
+
+            // We'll print out the usage, if requested.
+            if (configuration.printUsage != null)
+            {
+                System.out.println("Printing usage to [" + fileName(configuration.printUsage) + "]...");
+            }
+        }
+
+        // Perform the actual shrinking.
+        programClassPool =
+            new Shrinker(configuration).execute(programClassPool, libraryClassPool);
+    }
+
+
+    /**
+     * Performs the subroutine inlining step.
+     */
+    private void inlineSubroutines()
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Inlining subroutines...");
+        }
+
+        // Perform the actual inlining.
+        new SubroutineInliner(configuration).execute(programClassPool);
+    }
+
+
+    /**
+     * Performs the optimization step.
+     */
+    private boolean optimize() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Optimizing...");
+        }
+
+        // Perform the actual optimization.
+        return new Optimizer(configuration).execute(programClassPool, libraryClassPool);
+    }
+
+
+    /**
+     * Performs the obfuscation step.
+     */
+    private void obfuscate() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Obfuscating...");
+
+            // We'll apply a mapping, if requested.
+            if (configuration.applyMapping != null)
+            {
+                System.out.println("Applying mapping [" + fileName(configuration.applyMapping) + "]");
+            }
+
+            // We'll print out the mapping, if requested.
+            if (configuration.printMapping != null)
+            {
+                System.out.println("Printing mapping to [" + fileName(configuration.printMapping) + "]...");
+            }
+        }
+
+        // Perform the actual obfuscation.
+        new Obfuscator(configuration).execute(programClassPool, libraryClassPool);
+    }
+
+
+    /**
+     * Disambiguates the line numbers of all program classes, after
+     * optimizations like method inlining and class merging.
+     */
+    private void linearizeLineNumbers()
+    {
+        programClassPool.classesAccept(new LineNumberLinearizer());
+    }
+
+
+    /**
+     * Trims the line number table attributes of all program classes.
+     */
+    private void trimLineNumbers()
+    {
+        programClassPool.classesAccept(new AllAttributeVisitor(true,
+            new LineNumberTableAttributeTrimmer()));
+    }
+
+
+    /**
+     * Clears any JSE preverification information from the program classes.
+     */
+    private void clearPreverification()
+    {
+        programClassPool.classesAccept(
+            new ClassVersionFilter(ClassConstants.CLASS_VERSION_1_6,
+                new AllMethodVisitor(
+                    new AllAttributeVisitor(
+                        new NamedAttributeDeleter(ClassConstants.ATTR_StackMapTable)))));
+    }
+
+
+    /**
+     * Performs the preverification step.
+     */
+    private void preverify()
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Preverifying...");
+        }
+
+        // Perform the actual preverification.
+        new Preverifier(configuration).execute(programClassPool);
+    }
+
+
+    /**
+     * Sorts the elements of all program classes.
+     */
+    private void sortClassElements()
+    {
+        programClassPool.classesAccept(new ClassElementSorter());
+    }
+
+
+    /**
+     * Writes the output class files.
+     */
+    private void writeOutput() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Writing output...");
+        }
+
+        // Write out the program class pool.
+        new OutputWriter(configuration).execute(programClassPool);
+    }
+
+
+    /**
+     * Prints out the contents of the program classes.
+     */
+    private void dump() throws IOException
+    {
+        if (configuration.verbose)
+        {
+            System.out.println("Printing classes to [" + fileName(configuration.dump) + "]...");
+        }
+
+        PrintStream ps = createPrintStream(configuration.dump);
+        try
+        {
+            programClassPool.classesAccept(new ClassPrinter(ps));
+        }
+        finally
+        {
+            closePrintStream(ps);
+        }
+    }
+
+
+    /**
+     * Returns a print stream for the given file, or the standard output if
+     * the file name is empty.
+     */
+    private PrintStream createPrintStream(File file)
+        throws FileNotFoundException
+    {
+        return file == Configuration.STD_OUT ? System.out :
+            new PrintStream(
+                new BufferedOutputStream(
+                    new FileOutputStream(file)));
+    }
+
+
+    /**
+     * Closes the given print stream, or closes it if is the standard output.
+     * @param printStream
+     */
+    private void closePrintStream(PrintStream printStream)
+    {
+        if (printStream == System.out)
+        {
+            printStream.flush();
+        }
+        else
+        {
+            printStream.close();
+        }
+    }
+
+
+    /**
+     * Returns the canonical file name for the given file, or "standard output"
+     * if the file name is empty.
+     */
+    private String fileName(File file)
+    {
+        if (file == Configuration.STD_OUT)
+        {
+            return "standard output";
+        }
+        else
+        {
+            try
+            {
+                return file.getCanonicalPath();
+            }
+            catch (IOException ex)
+            {
+                return file.getPath();
+            }
+        }
+    }
+
+
+    /**
+     * The main method for ProGuard.
+     */
+    public static void main(String[] args)
+    {
+        if (args.length == 0)
+        {
+            System.out.println(VERSION);
+            System.out.println("Usage: java proguard.ProGuard [options ...]");
+            System.exit(1);
+        }
+
+        // Create the default options.
+        Configuration configuration = new Configuration();
+
+        try
+        {
+            // Parse the options specified in the command line arguments.
+            ConfigurationParser parser = new ConfigurationParser(args,
+                System.getProperties());
+            try
+            {
+                parser.parse(configuration);
+            }
+            finally
+            {
+                parser.close();
+            }
+
+            // Execute ProGuard with these options.
+            new ProGuard(configuration).execute();
+        }
+        catch (Exception ex)
+        {
+            if (configuration.verbose)
+            {
+                // Print a verbose stack trace.
+                ex.printStackTrace();
+            }
+            else
+            {
+                // Print just the stack trace message.
+                System.err.println("Error: "+ex.getMessage());
+            }
+
+            System.exit(1);
+        }
+
+        System.exit(0);
+    }
+}
